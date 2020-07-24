2412ce8dc20f5e239af38c6e2619897385072061
vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2020-07-24 08:11:06 -0300

    includ render_template and viewl-festa

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+from festa.views import festa
+app.register_blueprint(festa)
