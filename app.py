a74f5f0cef38deef80e98b6261d499b6ccb9c1a3
vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2020-08-16 21:29:37 -0300

    add app

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 Bootstrap(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+from auth import bp as auth_bp
+app.register_blueprint(auth_bp)
 
 
-from festa.views import festa
-app.register_blueprint(festa)
+from festa import bp as festa_bp
+app.register_blueprint(festa_bp, url_prefix='/convidados')
