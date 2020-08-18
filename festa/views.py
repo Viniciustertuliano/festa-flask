3c64c4fda85b4a999a14367b2a4144aa3d091902
Vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2020-08-18 04:02:23 +0000

    remove

--- festa/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-festa = Blueprint('festa', __name__, url_prefix='/convidados',
-                  template_folder='templates')
-
-
-@festa.route('/')
-def conv():
-    return render_template('festa.html')
