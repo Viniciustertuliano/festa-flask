43c1a79f5a0aa1b77e76ec4a364826520d14808b
ViniciusTertuliano <viniciustertuliano@arch.localdomain>
2020-10-02 04:43:40 -0300

    Removendo rota auth

--- app.py
+++ app.py
@@ -11,9 +11,5 @@ migrate = Migrate(app, db)
 Bootstrap(app)
 
 
-from auth import bp as auth_bp
-app.register_blueprint(auth_bp)
-
-
 from festa import bp as festa_bp
 app.register_blueprint(festa_bp, url_prefix='/convidados')
