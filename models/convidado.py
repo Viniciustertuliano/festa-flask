61193d49d19ff1b1c0942b53a57d66b2e24cd3e5
ViniciusTertuliano <viniciustertuliano@arch.localdomain>
2020-10-02 04:50:02 -0300

    Removido regra de Unique

--- models/convidado.py
+++ models/convidado.py
@@ -4,7 +4,7 @@ from app import db
 class Convidado(db.Model):
     __tablename__ = 'convidados'
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(20), index=True, unique=True)
+    nome = db.Column(db.String(20), index=True)
     acompanhante = db.Column(db.Integer)
 
     def __repr__(self):
